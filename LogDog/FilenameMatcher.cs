3771f498f5a43331dbd92a25acc89e3cfa933022
GraemeB <grae22@gmail.com>
2017-07-22 21:28:58 +0200

    Adding FilenameMatcher.

---
+++ LogDog/FilenameMatcher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LogDog
+{
+  internal class FilenameMatcher
+  {
+    //-------------------------------------------------------------------------
+
+    public static string ExtractBaseFilename(string filename)
+    {
+      var filenameWithoutExtension = Path.GetFileNameWithoutExtension(filename);
+
+      // We assume numbers are not important to the name (probably date/time),
+      // so we just take the letters, dashes, underscores, etc.
+      var tmp = string.Empty;
+
+      filenameWithoutExtension
+        .TakeWhile(x => char.IsLetter(x) || char.IsPunctuation(x))
+        .ToList()
+        .ForEach(x => tmp += x);
+
+      // Remove any trailing dashes, underscores, etc.
+      var tmpReversed = new string(tmp.Reverse().ToArray());
+
+      tmpReversed =
+        new string(
+          tmpReversed.SkipWhile(char.IsPunctuation).ToArray());
+
+      return
+        new string(
+          tmpReversed.Reverse().ToArray());
+    }
+
+    //-------------------------------------------------------------------------
+
+    public string BaseFilename { get; }
+
+    private readonly string _baseFilenameLower;
+
+    //-------------------------------------------------------------------------
+
+    public FilenameMatcher(string filename)
+    {
+      BaseFilename = ExtractBaseFilename(filename);
+
+      _baseFilenameLower = BaseFilename.ToLower();
+    }
+
+    //-------------------------------------------------------------------------
+
+    public bool Matches(string otherFilename)
+    {
+      var otherBaseFilename = ExtractBaseFilename(otherFilename);
+
+      return BaseFilename.Equals(otherBaseFilename, StringComparison.OrdinalIgnoreCase);
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
---
+++ LogDog_Test/FilenameMatcher_Test.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using LogDog;
+
+namespace LogDog_Test
+{
+  [TestFixture]
+  [Category("FilenameMatcher")]
+  class FilenameMatcher_Test
+  {
+    //-------------------------------------------------------------------------
+
+    private FilenameMatcher _testObject;
+
+    //-------------------------------------------------------------------------
+
+    [SetUp]
+    public void SetUp()
+    {
+      _testObject = new FilenameMatcher("Test-Object_2017-07-22.log");
+    }
+
+    //-------------------------------------------------------------------------
+
+    [Test]
+    public void ExtractsBaseFilename()
+    {
+      Assert.AreEqual(
+        "Test-Object",
+        FilenameMatcher.ExtractBaseFilename("Test-Object_2017-07-22.log"));
+    }
+
+    //-------------------------------------------------------------------------
+
+    [Test]
+    public void BaseFilename()
+    {
+      Assert.AreEqual("Test-Object", _testObject.BaseFilename);
+    }
+
+    //-------------------------------------------------------------------------
+
+    [Test]
+    public void ShouldMatch()
+    {
+      Assert.True(_testObject.Matches("test-object_2017-07-23.log"));
+    }
+
+    //-------------------------------------------------------------------------
+
+    [Test]
+    public void ShouldNotMatch()
+    {
+      Assert.False(_testObject.Matches("test-object-special_2017-07-23.log"));
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
