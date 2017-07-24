0c42251dbb94bf16da0004da4b95f58c99238080
GraemeB <grae22@gmail.com>
2017-07-24 21:04:17 +0200

    Use host qualified name.

--- LogDog/VersionedFileCollection.cs
+++ LogDog/VersionedFileCollection.cs
@@ -25,9 +25,11 @@ namespace LogDog
       string baseFilename = FilenameMatcher.ExtractBaseFilename(file.Path);
       string baseFilenameLower = FilenameMatcher.ExtractHostQualifiedBaseFilename(file).ToLower();
 
+      var qualifiedBaseFilename = $"{file.HostName}.{baseFilename}";
+
       if (_files.ContainsKey(baseFilenameLower) == false)
       {
-        _files.Add(baseFilenameLower, new VersionedFile(baseFilename));
+        _files.Add(baseFilenameLower, new VersionedFile(qualifiedBaseFilename));
       }
 
       _files[baseFilenameLower].AddVersion(file, suppressFileAddedEvent);
