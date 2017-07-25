91b71178693f37af15eb57c1d8292b7ffa7552ab
graemeb <grae22@gmail.com>
2017-07-25 07:37:53 +0200

    Continuous update thread (much room for optimisation here in terms of file scanning).

--- LogDog/Program.cs
+++ LogDog/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.IO;
 using System.IO.Abstractions;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace LogDog
 {
   public static class Program
   {
+    //-------------------------------------------------------------------------
+
+    private static Thread _runner;
+    private static bool _runnerIsAlive;
+    private static NotifyIcon _systemTrayIcon;
+
     //-------------------------------------------------------------------------
 
     [STAThread]
@@ -17,7 +24,7 @@ namespace LogDog
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
 
-        var systemTrayIcon = new NotifyIcon()
+        _systemTrayIcon = new NotifyIcon()
         {
           ContextMenu = new ContextMenu(),
           Icon = Resources.icon,
@@ -25,11 +32,14 @@ namespace LogDog
           Visible = true
         };
 
-        BuildContextMenu(systemTrayIcon.ContextMenu);
+        AddExitOption(_systemTrayIcon.ContextMenu);
+
+        InitialiseRunner();
 
         Application.Run();
 
-        systemTrayIcon.Dispose();
+        _systemTrayIcon.Dispose();
+        _systemTrayIcon = null;
       }
       catch (Exception e)
       {
@@ -45,8 +55,75 @@ namespace LogDog
 
     //-------------------------------------------------------------------------
 
-    private static void BuildContextMenu(ContextMenu menu)
+    private static void Shutdown()
     {
+      _runnerIsAlive = false;
+
+      if (_runner?.Join(1000) == false)
+      {
+        _runner.Abort();
+      }
+
+      Application.Exit();
+    }
+
+    //-------------------------------------------------------------------------
+
+    private static void InitialiseRunner()
+    {
+      var threadStart = new ThreadStart(Run);
+      _runner = new Thread(threadStart);
+      _runner.Start();
+    }
+
+    //-------------------------------------------------------------------------
+
+    private static void Run()
+    {
+      var setContextMenu = new SetContextMenuDelegate(SetContextMenu);
+
+      _runnerIsAlive = true;
+
+      while (_runnerIsAlive)
+      {
+        try
+        {
+          Cursor.Current = Cursors.WaitCursor;
+          ContextMenu menu = BuildContextMenu();
+          Cursor.Current = Cursors.Default;
+
+          setContextMenu.Invoke(menu);
+
+          Thread.Sleep(1000 * 60 * 10);
+        }
+        catch (ThreadInterruptedException)
+        {
+          // Ignore.
+        }
+      }
+    }
+
+    //-------------------------------------------------------------------------
+
+    private delegate void SetContextMenuDelegate(ContextMenu menu);
+
+    private static void SetContextMenu(ContextMenu menu)
+    {
+      if (_systemTrayIcon == null)
+      {
+        return;
+      }
+
+      _systemTrayIcon.ContextMenu?.Dispose();
+      _systemTrayIcon.ContextMenu = menu;
+    }
+
+    //-------------------------------------------------------------------------
+
+    private static ContextMenu BuildContextMenu()
+    {
+      ContextMenu menu = new ContextMenu();
+
       var hosts = new WindowsHostsFile(
         Environment.ExpandEnvironmentVariables(Settings.Default.HostsFilename),
         Settings.Default.HostFileBlockStart,
@@ -95,11 +172,20 @@ namespace LogDog
         menu.MenuItems.Add(subMenu.MenuItem);
       }
 
+      AddExitOption(menu);
+
+      return menu;
+    }
+
+    //-------------------------------------------------------------------------
+
+    private static void AddExitOption(ContextMenu menu)
+    {
       menu.MenuItems.Add("-");
       menu.MenuItems.Add(
         new MenuItem(
           "E&xit",
-          (sender, args) => { Application.Exit(); }));
+          (sender, args) => { Shutdown(); }));
     }
 
     //-------------------------------------------------------------------------
